155d1d6b7bff1aad1dc405935604b2697d40d1b8
lucas.silva <lucas.silva@jeaholding.com.br>
2023-07-17 11:09:26 -0300

    Updated code

--- src/funcpipe/__init__.py
+++ src/funcpipe/__init__.py
@@ -1,13 +1,4 @@
-from dataclasses import dataclass, field
-from typing import (
-    Any,
-    Callable,
-    Generic,
-    NoReturn,
-    Optional,
-    Tuple,
-    TypeVar,
-)
+from typing import Any, Callable, Generic, NoReturn, Optional, Tuple, TypeVar
 
 
 def rraise(_: Any) -> NoReturn:
@@ -24,17 +15,13 @@ class Pipe(Generic[INITIAL_T, FINAL_T]):
     def __init__(self, func: Callable[[INITIAL_T], FINAL_T]) -> None:
         self.func = func
 
-    def __rshift__(
-        self, other: Callable[[FINAL_T], NEW_FINAL_T]
-    ) -> "Pipe[INITIAL_T, NEW_FINAL_T]":
+    def __rshift__(self, other: Callable[[FINAL_T], NEW_FINAL_T]) -> "Pipe[INITIAL_T, NEW_FINAL_T]":
         return Pipe(lambda value: other(self.func(value)))
 
     def __call__(self, value: INITIAL_T) -> FINAL_T:
         return self.func(value)
 
-    def pipe(
-        self, other: Callable[[FINAL_T], NEW_FINAL_T]
-    ) -> "Pipe[INITIAL_T, NEW_FINAL_T]":
+    def pipe(self, other: Callable[[FINAL_T], NEW_FINAL_T]) -> "Pipe[INITIAL_T, NEW_FINAL_T]":
         return Pipe(lambda value: other(self.func(value)))
 
 
@@ -46,12 +33,16 @@ TRANSFORMER = Callable[[T], J]
 
 def pattern_match(
     cases: Tuple[Tuple[PREDICATE[T], TRANSFORMER[T, J]], ...],
+    default: Optional[TRANSFORMER[T, J]] = None,
 ) -> Pipe[T, J]:
     @Pipe
     def _pattern_match(value: T) -> J:
         for case, transform in cases:
             if case(value):
                 return transform(value)
+
+        if default is not None:
+            return default(value)
         raise ValueError("No case matched")
 
     return _pattern_match
